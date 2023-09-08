4bb730867db576949635be9b568db3543da64c14
Archer-du <dupengche@gmail.com>
2023-09-08 13:21:14 +0800

    initialize

---
+++ init.lua
@@ -0,0 +1,29 @@
+vim.g.neovide_transparency = 0.99
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_refresh_rate = 60
+
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+
+require('vim.keymap')
+vim.api.nvim_create_autocmd('VimEnter', {callback = function() vim.cmd('Nvdash') end})
